baeee5e306faa3358236c3d41109d3ac5faff013
Frank Espinoza <frank1392@gmail.com>
2024-02-03 17:30:30 +0930

    more changes

--- LeaveManagement.Web/Controllers/LeaveRequestsController.cs
+++ LeaveManagement.Web/Controllers/LeaveRequestsController.cs
@@ -12,113 +12,115 @@ using LeaveManagement.Web.Contracts;
 using Microsoft.AspNetCore.Authorization;
 using LeaveManagement.Web.Constants;
 
-namespace LeaveManagement.Web.Controllers
+namespace LeaveManagement.Web.Controllers;
+
+[Authorize]
+public class LeaveRequestsController : Controller
 {
-    [Authorize]
-    public class LeaveRequestsController : Controller
+    private readonly ILeaveRequestRepository leaveRequestRepository;
+    private readonly ILeaveTypeRepository leaveTypeRepository;
+
+    public LeaveRequestsController(ILeaveRequestRepository leaveRequestRepository,
+        ILeaveTypeRepository leaveTypeRepository)
     {
-        private readonly ILeaveRequestRepository leaveRequestRepository;
-        private readonly ILeaveTypeRepository leaveTypeRepository;
+        this.leaveRequestRepository = leaveRequestRepository;
+        this.leaveTypeRepository = leaveTypeRepository;
+    }
 
-        public LeaveRequestsController(ILeaveRequestRepository leaveRequestRepository, ILeaveTypeRepository leaveTypeRepository)
+    [Authorize(Roles = Roles.Administrator)]
+    // GET: LeaveRequests
+    public async Task<IActionResult> Index()
+    {
+        var model = await leaveRequestRepository.GetAdminLeaveRequestList();
+        return View(model);
+    }
+
+    public async Task<ActionResult> MyLeave()
+    {
+        var model = await leaveRequestRepository.GetMyLeaveDetails();
+        return View(model);
+    }
+
+    // GET: LeaveRequests/Details/5
+    public async Task<IActionResult> Details(int? id)
+    {
+        var model = await leaveRequestRepository.GetLeaveRequestAsync(id);
+        if (model == null)
         {
-            this.leaveRequestRepository = leaveRequestRepository;
-            this.leaveTypeRepository = leaveTypeRepository;
+            return NotFound();
         }
 
-        [Authorize(Roles = Roles.Administrator)]
-        // GET: LeaveRequests
-        public async Task<IActionResult> Index()
+        return View(model);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> ApproveRequest(int id, bool approved)
+    {
+        try
         {
-            var model = await leaveRequestRepository.GetAdminLeaveRequestList();
-            return View(model);
+            await leaveRequestRepository.ChangeApprovalStatus(id, approved);
+        }
+        catch (Exception ex)
+        {
+            throw;
         }
 
-        public async Task<ActionResult> MyLeave()
+        return RedirectToAction(nameof(Index));
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Cancel(int id)
+    {
+        try
         {
-            var model = await leaveRequestRepository.GetMyLeaveDetails();
-            return View(model);
+            await leaveRequestRepository.CancelLeaveRequest(id);
+        }
+        catch (Exception ex)
+        {
+            throw;
         }
 
-        // GET: LeaveRequests/Details/5
-        public async Task<IActionResult> Details(int? id)
+        return RedirectToAction(nameof(MyLeave));
+    }
+
+    // GET: LeaveRequests/Create
+    public async Task<IActionResult> Create()
+    {
+        var model = new LeaveRequestCreateVM
         {
-            var model = await leaveRequestRepository.GetLeaveRequestAsync(id);
-            if(model == null)
-            {
-                return NotFound();
-            }
-            return View(model);
-        }
+            LeaveTypes = new SelectList(await leaveTypeRepository.GetAllAsync(), "Id", "Name")
+        };
+        return View(model);
+    }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> ApproveRequest(int id, bool approved)
+    // POST: LeaveRequests/Create
+    // To protect from overposting attacks, enable the specific properties you want to bind to.
+    // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(LeaveRequestCreateVM model)
+    {
+        try
         {
-            try
+            if (ModelState.IsValid)
             {
-                await leaveRequestRepository.ChangeApprovalStatus(id, approved);
-            }
-            catch (Exception ex)
-            {
-
-                throw;
-            }
-            return RedirectToAction(nameof(Index));
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Cancel(int id)
-        {
-            try
-            {
-                await leaveRequestRepository.CancelLeaveRequest(id);
-            }
-            catch (Exception ex)
-            {
-
-                throw;
-            }
-            return RedirectToAction(nameof(MyLeave));
-        }
-
-        // GET: LeaveRequests/Create
-        public async Task<IActionResult> Create()
-        {
-            var model = new LeaveRequestCreateVM
-            {
-                LeaveTypes = new SelectList(await leaveTypeRepository.GetAllAsync(), "Id", "Name")
-            };
-            return View(model);
-        }
-
-        // POST: LeaveRequests/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create(LeaveRequestCreateVM model)
-        {
-            try
-            {
-                if (ModelState.IsValid)
+                var isValidRequest = await leaveRequestRepository.CreateLeaveRequest(model);
+                if (isValidRequest)
                 {
-                    var isValidRequest = await leaveRequestRepository.CreateLeaveRequest(model);
-                    if(isValidRequest)
-                    {
-                        return RedirectToAction(nameof(MyLeave));
-                    }
-                    ModelState.AddModelError(string.Empty, "You have exceeded your allocation with this request.");
+                    return RedirectToAction(nameof(MyLeave));
                 }
+
+                ModelState.AddModelError(string.Empty, "You have exceeded your allocation with this request.");
             }
-            catch (Exception ex)
-            {
-                ModelState.AddModelError(string.Empty, "An Error Has Occurred. Please Try Again Later");
-            }
-            
-            model.LeaveTypes = new SelectList(await leaveTypeRepository.GetAllAsync(), "Id", "Name", model.LeaveTypeId);
-            return View(model);
         }
+        catch (Exception ex)
+        {
+            ModelState.AddModelError(string.Empty, "An Error Has Occurred. Please Try Again Later");
+        }
+
+        model.LeaveTypes = new SelectList(await leaveTypeRepository.GetAllAsync(), "Id", "Name", model.LeaveTypeId);
+        return View(model);
     }
 }
